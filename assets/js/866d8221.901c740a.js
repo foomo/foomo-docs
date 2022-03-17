d1b7e377c1a6b224a76b582e143a3d92e0b29145
Nicola Turcato <nicola.turcato@bestbytes.com>
2022-03-17 11:13:39 +0100

    Deploy website - based on c700a890f2ff7d6c203b60f6c30bf053076c511a

---
+++ assets/js/866d8221.901c740a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfoomo=self.webpackChunkfoomo||[]).push([[2471],{6287:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"/blog/tags/bundle-size","name":"bundle size","count":1,"permalink":"/blog/tags/bundle-size"}')}}]);
